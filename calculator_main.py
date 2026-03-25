7cb19618712f8066939c2cdd4cd63b2a6c8f72cf
anton <121177791+Toenns@users.noreply.github.com>
2026-03-25 09:16:39 +0100

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -73,3 +73,56 @@ def calculate_dynamic(wp_bedarf, pv_neigung, pv_ausrichtung, pv_kwp, ea_wochenta
         summe += 113.82
     summe = round(summe, 2)
     print(summe)
+    return summe
+
+def calculate_static(wp_bedarf, pv_neigung, pv_ausrichtung, pv_kwp, ea_wochentag, ea_wochenende, ea_verbrauch, ea_leistung, ea_beginn):
+    pv_datei_name = "2025_15min_pv-ertrag.csv"
+
+    mein_preis_arraz = []
+    mein_wp_array = wp.berechne_waermepumpe_verbrauch(temp_datei="2025_15min_temperaturverlauf.csv", t_base=15.0, jahresbedarf=wp_bedarf, verbose=False)
+    mein_pv_array = pv.pv_erstellen(pv_neigung, pv_ausrichtung, pv_kwp)
+    mein_ea_array = ea.generiere_lade_profil(fahrleistung_woche_tag_km=ea_wochentag, fahrleistung_wochenende_tag_km=ea_wochenende, verbrauch_pro_100km=ea_verbrauch, wallbox_leistung_kw=ea_leistung, ladebeginn_stunde=ea_beginn)
+    
+    for i in range(35040)
+        mein_preis_array.append(32.4)
+    
+    mein_haushalt_array = ha.generate_yearly_profile_2025()
+    #einspeisevergütung
+    for i in range(len(mein_pv_array)):
+        verbrauch = mein_wp_array[i] + mein_ea_array[i] + mein_haushalt_array[i]
+
+        bilanz = verbrauch - (mein_pv_array[i] * 0.25)
+
+        if bilanz > 0.0:
+
+            if 0 < speicher_ladung < bilanz and speicher_leistung*0.25 > speicher_ladung:
+                bilanz -= speicher_ladung * 0.9
+                speicher_ladung = 0
+            elif speicher_ladung > 0 and bilanz > speicher_leistung*0.25 and speicher_leistung*0.25 < speicher_ladung:
+                bilanz -= speicher_leistung*0.25
+                speicher_ladung -= (speicher_leistung*0.25)/0.9
+            elif speicher_ladung > 0 and bilanz < speicher_leistung*0.25 and bilanz/0.9 < speicher_ladung:
+                speicher_ladung -= bilanz/0.9
+                bilanz = 0
+            summe += bilanz * mein_preis_array[i]
+            abnahme += 1
+
+        elif bilanz < 0.0:
+            if speicher_ladung < speicher_max:
+                speicher_ladung -= bilanz*0.9
+                print("Ladung von ", bilanz*-1)
+                if speicher_ladung >= speicher_max:
+                    speicher_ladung = speicher_max
+                print("Aktuelle Ladung: ", speicher_ladung)
+            else:
+                print("Einspeise: ", bilanz * einspeiseverguetung)
+                summe += bilanz * einspeiseverguetung
+                print("Summe: ", summe)
+                einspeise += 1
+
+    summe = summe / 100 #Von cent in euro Umrechnen
+    summe += 123.09
+    summe = round(summe, 2)
+    print(summe)
+    return summe
+
