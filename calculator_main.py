95c8f68a4fa42bd8004f7df0a42d98ae0dbedb9c
anton <121177791+Toenns@users.noreply.github.com>
2026-03-25 11:36:06 +0100

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -27,7 +27,13 @@ def calculate_dynamic(wp_bedarf, pv_neigung, pv_ausrichtung, pv_kwp, ea_wochenta
     preis_datei_name = "2025_15min_spotmarktpreis.csv"
 
     mein_wp_array = wp.berechne_waermepumpe_verbrauch(temp_datei="2025_15min_temperaturverlauf.csv", t_base=15.0, jahresbedarf=wp_bedarf, verbose=False)
-    mein_pv_array = pv.pv_erstellen(pv_neigung, pv_ausrichtung, pv_kwp)
+    if pv_kwp != 0:    
+        mein_pv_array = pv.pv_erstellen(pv_neigung, pv_ausrichtung, pv_kwp)
+    else:
+        mein_pv_array.clear()
+        for i in range(35040):
+            mein_pv_array.append(0)
+    
     mein_ea_array = ea.generiere_lade_profil(fahrleistung_woche_tag_km=ea_wochentag, fahrleistung_wochenende_tag_km=ea_wochenende, verbrauch_pro_100km=ea_verbrauch, wallbox_leistung_kw=ea_leistung, ladebeginn_stunde=ea_beginn)
     mein_preis_array = lade_strompreise_als_array(preis_datei_name)
     mein_haushalt_array = ha.verbrauch(ha_verbrauch)
